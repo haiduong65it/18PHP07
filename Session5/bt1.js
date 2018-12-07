dc0698803cd686695e16e8b9235393020eb55bca
Hai Duong <haiduong.if@gmail.com>
2018-12-07 19:00:35 +0700

    Nop bai JS

---
+++ Session5/bt1.js
@@ -0,0 +1,22 @@
+var a=5;
+var b=6;
+function summation(a, b){
+    document.write(a +" + " +b+ " = "+(a+b));
+    document.write("<br>");
+}
+function Subtraction(a, b){
+    document.write(a +" - " +b+ " = "+(a-b));
+    document.write("<br>");
+}
+function multiplication(a, b){
+    document.write(a +" * " +b+ " = "+(a*b));
+    document.write("<br>");
+}
+function division(a, b){
+    document.write(a +" / " +b+ " = "+(a/b));
+    document.write("<br>");
+}
+summation(a, b);
+Subtraction(a, b);
+multiplication(a, b);
+division(a, b);
