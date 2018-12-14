c99b35311d3f4c34475182b635fa216139c46c2c
Hai Duong <haiduong.if@gmail.com>
2018-12-14 18:32:13 +0700

    Sua bai

--- Session7/learn_js6.js
+++ Session7/learn_js6.js
@@ -1,18 +1,22 @@
-var a;
-var b;
-var c;
-var d;
-function Max(a, b, c, d) {
-    (a > b & a > c & a > d)?document.write(a + " là số lớn nhất<br>"):
-        (b>c & b > d)?document.write(b + " là số lớn nhất<br>"):
-            (c > d)?document.write(c + " là số lớn nhất<br>"): 
-                document.write(d + " là số lớn nhất<br>"); 
+var m = [2,5,15,7];
+function Max(m) {
+    var max = m[0];
+    for (i = 1;i < m.length; i++){
+        if (m[i] > max)
+            max = m[i];
+    }
+    document.write(max + " là số lớn nhất<br>");
 }
-function Min(a, b, c, d) {
-    (a < b & a < c & a < d)?document.write(a + " là số nhỏ nhất<br>"):
-        (b < c & b < d)?document.write(b + " là số nhỏ nhất<br>"):
-            (c < d)?document.write(c + " là số nhỏ nhất<br>"): 
-                document.write(d + " là số nhỏ nhất<br>"); 
+function Min(m) {
+    var min = m[0];
+    for (i = 1;i < m.length; i++){
+        if (m[i] < min)
+            min = m[i];
+    }
+    document.write(min + " là số bé nhất<br>");
 }
-Max(2, 5, 15, 7);
-Min(2, 5, 15, 7);
+Max(m);
+Min(m);
+
+
+
